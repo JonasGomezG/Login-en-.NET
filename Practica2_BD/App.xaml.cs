f1dee71c9bbf56b65fbc8a65a1bd0100df5c2081
Jonas <jonas@gmail.com>
2023-11-20 11:20:25 +0100

    Agregar archivos de proyecto.

---
+++ Practica2_BD/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Practica2_BD
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Practica2_BD/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Practica2_BD
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
